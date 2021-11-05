6e846262f241e0f27bd1feabee5e8fbf71dc16f5
hancush <hannah.cushman@gmail.com>
2021-11-05 10:31:39 -0500

    Omit attachments that are not of the expected filetypes

--- scripts/document_link.py
+++ scripts/document_link.py
@@ -6,17 +6,15 @@ import scrapelib
 
 
 def Link(url):
+
     if url.startswith('https://metro.legistar.com/ViewReport.ashx'):
         return BoardReportLink(url)
 
     return DocumentLink(url)
 
 
-class DocumentLink(object):
-    '''
-    https://metro.legistar1.com/metro/meetings/2021/10/2100_A_Operations%2C_Safety%2C_and_Customer_Experience_Committee_21-10-21_Agenda.pdf
-    http://metro.legistar1.com/metro/attachments/6484cdc7-3c2a-4598-abc5-9d846771158e.pdf
-    '''
+class _Link(object):
+
     def __init__(self, url):
         self.url = url
 
@@ -28,6 +26,20 @@ class DocumentLink(object):
 
     @property
     def filename(self):
+        raise NotImplementedError()
+
+    @property
+    def filetype(self):
+        return os.path.splitext(self.filename)[-1]
+
+
+class DocumentLink(_Link):
+    '''
+    https://metro.legistar1.com/metro/meetings/2021/10/2100_A_Operations%2C_Safety%2C_and_Customer_Experience_Committee_21-10-21_Agenda.pdf
+    http://metro.legistar1.com/metro/attachments/6484cdc7-3c2a-4598-abc5-9d846771158e.pdf
+    '''
+    @property
+    def filename(self):
         '''
         Return the filename from the URL, less any percent signs from escaped
         characters (commas, spaces, etc.), as these will confuse Make.
@@ -35,7 +47,7 @@ class DocumentLink(object):
         return re.sub(r'%', '', os.path.basename(self.url))
 
 
-class BoardReportLink(DocumentLink):
+class BoardReportLink(_Link):
     '''
     https://metro.legistar.com/ViewReport.ashx?M=R&N=TextL5&GID=557&ID=7936&GUID=LATEST&Title=Board+Report
     '''
--- scripts/download_attachments.py
+++ scripts/download_attachments.py
@@ -10,12 +10,25 @@ from document_link import Link
 
 
 if __name__ == '__main__':
+    ACCEPTED_FILETYPES = (
+        '.pdf',
+        '.xlsx',
+        '.doc',
+        '.docx',
+        '.ppt',
+        '.pptx',
+        '.rtf',
+    )
+
     filenames = []
 
     for attachment_link in json.loads(
         os.environ['attachment_links'].replace('\'', '"')
     ):
         link = Link(attachment_link)
+
+        if link.filetype not in ACCEPTED_FILETYPES:
+            continue
 
         with open(os.path.join('attachments', link.filename), 'wb') as file:
             file.write(link.content)
